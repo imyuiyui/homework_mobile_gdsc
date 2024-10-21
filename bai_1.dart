9fd083165adca2fd1ece65c965785c48fce31bd6
imyuiyui <noobmail0301@gmail.com>
2024-10-21 23:32:37 +0700

    test

--- bai_1.dart
+++ bai_1.dart
@@ -1,5 +1,17 @@
 void main() {
   List<int> numbers = [10,5,7,1,2,9,4,3,6,8];
   
-  // your awnser 
+  List<int> sl = List.from(numbers)..sort();
+  int so3 = sl[sl.length-3];
+  int vitri = numbers.indexOf(so3);
+  print(vitri);
+  
+  List<int> hon4 = numbers.where((a) => a > 4).toList();
+  print(hon4);
+  
+  int sum = numbers.reduce((a,b)=> a+b);
+  int maxx = numbers.where((c) => c % 2 != 0).reduce((a, b) => a > b ? a : b);
+  int sth = numbers.indexOf(maxx);
+  numbers[sth] = sum;
+  print(numbers);
 }
